f9eefb796b53753b36fb42be8199281f1a60c3ff
katoon1142-404 <katoon1142@gmail.com>
2026-08-24 17:15:25 +0700

    AddLab2PlanA

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,4 +1,10 @@
-﻿
+﻿/*
+ * Student ID : 1690701758
+ * Name       : Ornicha pomnoi
+ * Section    : 129B
+ * No.        : N/A
+ * Course     : GI113 Computer Programming (GI)
+ */
 
 namespace Lab02
 {
@@ -6,7 +12,36 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
             Console.WriteLine("Hello, World!");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank:{rank}");
+            Console.WriteLine($"HP:{currentHp}/{maxHp}");
+            Console.WriteLine($"Attack Power: {attackPower}");
+            Console.WriteLine($"Crit Mulipier: {critMultiplier}");
+            Console.WriteLine($"Is Boss:{isBoss}");
+            Console.WriteLine();
+
+            int hpPercent = currentHp * 100 / maxHp; //คำนวณเปอร์เซ็น
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+
+            Console.WriteLine($"Kirin takes 60 damage!");
+            currentHp = currentHp - 60;
+            Console.WriteLine();
+
+            Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"HP: {currentHp} / {maxHp}");
+            hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
         }
     }
 }
