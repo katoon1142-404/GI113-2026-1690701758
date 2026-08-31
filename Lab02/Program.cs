99406de3fabc04ce098f4c5d0091ca0c08c1218e
katoon1142-404 <katoon1142@gmail.com>
2026-08-31 16:51:35 +0700

    Lab03

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -42,6 +42,62 @@ namespace Lab02
             Console.WriteLine($"HP: {currentHp} / {maxHp}");
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
+            //รักเกย์เสมอ ไม่เจอก็รัก
+
+            string hero1Name = "lucian";
+            int hero1Hp = 240; 
+            float hero1Atk = 50f;
+            double hero1CritRate = 2.00;
+            char hero1Rank = 'S';
+
+            Console.WriteLine("====HERO S====");
+            Console.WriteLine($"NAME: {hero1Name}");
+            Console.WriteLine($"HP: {hero1Hp}");
+            Console.WriteLine($"Attack Power: {hero1Atk}");
+            Console.WriteLine($"Crit Rate: {hero1CritRate}");
+            Console.WriteLine($"Rank: {hero1Rank}");
+
+
+            string hero2Name = "zudan";
+            int hero2Hp = 90;
+            float hero2Atk = 70f;
+            double hero2CritRate = 1;
+            char hero2Rank = 'A';
+
+            Console.WriteLine("====HERO A====");
+            Console.WriteLine($"NAME: {hero2Name}");
+            Console.WriteLine($"HP: {hero2Hp}");
+            Console.WriteLine($"Attack Power: {hero2Atk}");
+            Console.WriteLine($"Crit Rate: {hero2CritRate}");
+            Console.WriteLine($"Rank: {hero2Rank}");
+
+            string hero3Name = "moko";
+            int hero3Hp = 300;
+            float hero3Atk = 45.5f;
+            double hero3CritRate = 0.50;
+            char hero3Rank = 'A';
+
+            Console.WriteLine("====HERO A====");
+            Console.WriteLine($"NAME: {hero3Name}");
+            Console.WriteLine($"HP: {hero3Hp}");
+            Console.WriteLine($"Attack Power: {hero3Atk}");
+            Console.WriteLine($"Crit Rate: {hero3CritRate}");
+            Console.WriteLine($"Rank: {hero3Rank}");
+
+            string hero4Name = "C an";
+            int hero4Hp = 120;
+            float hero4Atk = 20.5f;
+            double hero4CritRate = 0.10;
+            char hero4Rank = '?';
+
+            Console.WriteLine("====HERO ?====");
+            Console.WriteLine($"NAME: {hero4Name}");
+            Console.WriteLine($"HP: {hero4Hp}");
+            Console.WriteLine($"Attack Power: {hero4Atk}");
+            Console.WriteLine($"Crit Rate: {hero4CritRate}");
+            Console.WriteLine($"Rank: {hero4Rank}");
+
+
         }
     }
 }
---
+++ Lab03/LAB03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace LAB03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MaxLevel = 10;
+
+            var bossName = "Kirin";   // ต้องประกาศด้วย var ห้ามเขียน string ตรงๆ
+            var rank = 'S';            // ต้องประกาศด้วย var ห้ามเขียน char ตรงๆ
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 115;       // ค่าตั้งต้นของ Lab นี้คือ HP "หลังโดนโจมตี" จาก Lab 2 แล้ว ไม่ใช่ 175
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            //ปร้ิ้น
+            Console.WriteLine("===== KIRIN SAVE CONVERTER =====");
+            Console.WriteLine($"Name: {bossName}"+
+            $"\nRANK: {rank}" +
+             $"\nLevel: {level}" +
+             $"\nMax HP: {maxHp}" +
+             $"\nCurrent HP: {currentHp}" +
+             $"\nAttack Power: {attackPower}" +
+             $"\nCritical Multiplier: {critMultiplier}" +
+             $"\nIs Boss: {isBoss}");
+
+            Console.WriteLine("----- Implicit Conversion: HP as double -----");
+            double currentHpAsDouble = currentHp;
+            Console.WriteLine($"HP (double): {currentHpAsDouble}");
+
+            Console.WriteLine("\n ----- Exact HP Percent (no interger truncation) -----");
+            double hpPercentExact = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent (exact): {hpPercentExact}%");
+
+            Console.WriteLine("\n----- Explicit Cast: Attack Power -> Display int -----");
+            int attackDisney = (int)attackPower;
+            Console.WriteLine($"Attack Power (int cast): {attackDisney}");
+
+            Console.WriteLine("\n---- - Cast vs Convert: Crit Multiplier-----");
+            int critCast = (int)critMultiplier;
+            int critConvert = Convert.ToInt32(critMultiplier);
+            Console.WriteLine($"Critical Multiplier (int cast): {critCast}");
+            Console.WriteLine($"Critical Multiplier (Convert.ToInt32): {critConvert}");
+        }
+    }
+}
